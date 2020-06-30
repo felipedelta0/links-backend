e2db51c98c6cc8b38a6dc59d57da70bdfa65de07
Luis Felipe <felipedelta0@gmail.com>
2020-06-29 21:30:26 -0300

    Storing accounts into database

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -1,13 +1,27 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+const { Account } = require('../models')
 
 const router = express.Router()
+
+const saltRounds = 8
 
 router.get('/signin', (req, res) => {
   return res.json('Sign in')
 })
 
-router.get('/signup', (req, res) => {
-  return res.json('Sign up')
+router.get('/signup', async (req, res) => {
+
+  const email = 'asadelta@delta.com'
+  const password = '123456'
+
+  const hash = bcrypt.hashSync(password, saltRounds)
+
+  const result = await Account.create({ email, password: hash })
+
+  console.log(result)
+
+  return res.json(result)
 })
 
 module.exports = router
