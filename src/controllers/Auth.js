5a7b552a9a67516eaff7c046bf589a3002696c57
Luis Felipe <felipedelta0@gmail.com>
2020-06-30 20:30:41 -0300

    jwt implementation

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -1,19 +1,30 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const { Account } = require('../models')
-const { accountSignUp } = require('../validators/Account')
+const { accountSignUp, accountSignIn } = require('../validators/Account')
 const { getMessage } = require('../helpers/Messages')
+const { generateJwt, generateRefreshJwt } = require('../helpers/jwt')
 
 const router = express.Router()
 
 const saltRounds = 8
 
-router.get('/signin', (req, res) => {
-  return res.jsonOK(null)
+router.post('/signin', accountSignIn, async (req, res) => {
+  const { email, password } = req.body
+
+  const account = await Account.findOne({ where: { email } })
+  const match = account ? bcrypt.compareSync(password, account.password) : null
+
+  if (!match)
+    return res.jsonBadRequest(account, getMessage('Account.signIn.invalid'))
+
+  const token = generateJwt({ id: account.id })
+  const refreshToken = generateRefreshJwt({ id: account.id })
+
+  return res.jsonOK(account, getMessage('Account.signIn.success'), { token, refreshToken })
 })
 
 router.post('/signup', accountSignUp, async (req, res) => {
-
   const { email, password } = req.body
 
   const account = await Account.findOne({ where: { email } })
@@ -23,7 +34,10 @@ router.post('/signup', accountSignUp, async (req, res) => {
   const hash = bcrypt.hashSync(password, saltRounds)
   const newAccount = await Account.create({ email, password: hash })
 
-  return res.jsonOK(newAccount, getMessage('Account.signUp.success'))
+  const token = generateJwt({ id: newAccount.id })
+  const refreshToken = generateRefreshJwt({ id: newAccount.id })
+
+  return res.jsonOK(newAccount, getMessage('Account.signUp.success'), { token, refreshToken })
 })
 
 module.exports = router
